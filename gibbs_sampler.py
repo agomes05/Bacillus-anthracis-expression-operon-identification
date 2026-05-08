447dac5e745f980975754d0f6681398de5f9c9dd
Arthur Gomes <aggomes05@gmail.com>
2026-05-08 18:00:01 -0400

    gibbs_sampler start

---
+++ gibbs_sampler.py
@@ -0,0 +1,52 @@
+# Implement the Gibbs sampler for motif finding in DNA sequences
+
+files_to_process = [
+#   (filename, species, open, sequence size)
+    ("B. anthracis sequences/BA-GERA250BP.fasta", "B. anthracis", "gerA", 250),
+    ("B. anthracis sequences/BA-GERA500BP.fasta", "B. anthracis", "gerA", 500),
+    ("B. anthracis sequences/BA-GERA100BP.fasta", "B. anthracis", "gerA", 1000),
+    ("B. anthracis sequences/BA-GERB250BP.fasta", "B. anthracis", "gerA", 250),
+    ("B. anthracis sequences/BA-GERB500BP.fasta", "B. anthracis", "gerA", 500),
+    ("B. anthracis sequences/BA-GERB1000BP.fasta", "B. anthracis", "gerA", 1000),
+    ("B. anthracis sequences/BA-GERK250BP.fasta", "B. anthracis", "gerA", 250),
+    ("B. anthracis sequences/BA-GERK500BP.fasta", "B. anthracis", "gerA", 500),
+    ("B. anthracis sequences/BA-GERK1000BP.fasta", "B. anthracis", "gerA", 1000),
+    (),
+    (),
+    (),
+    (),
+    (),
+    (),
+    (),
+    (),
+    (),
+]
+
+def read_fasta(filename: str) -> str:
+    """Read a FASTA file and return the concatenated DNA sequence as a string.
+    
+    Args:
+    filename (str): The path to the FASTA file.
+
+    Returns:
+        sequence (str): The concatenated DNA sequence.
+    """
+    
+    sequence = ""
+    with open(filename, "r") as file:
+        for line in file:
+            if line.startswith(">"):
+                continue  # Skip header lines
+            else:
+                sequence += line.strip()  # Remove whitespace and concatenate
+            
+    return sequence
+
+def gibbs_sampler(filename, species, operon, sequence_size):
+    """
+    
+    
+    """
+    all_results = []
+    
+    return all_results
